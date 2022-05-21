3e12f1f1362be6908c0309d93058b9e1a771dcd8
Pavel <rengetsu@mail.ru>
2022-05-21 23:43:28 +0300

    CameraMovement Update

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    float speed;                //  How fast the camera can move back and forward across the map
+    float zoomSpeed;            //  How fast the camera can scroll up and down
+    float rotateSpeed;          //  How fast the camera can spin around on it's axis
+
+    float maxHeight = 40f;      //  How high camera can go 
+    float minHeight = 4f;       //  How low camera can go
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,11 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float hsp = speed * Input.GetAxis("Horizontal");
+        float vsp = speed * Input.GetAxis("Vertical");
+        float scrollSp = -zoomSpeed * Input.GetAxis("Mouse Scrollwheel");
+
+
+        Vector3 verticalMove = new Vector3(0, scrollSp, 0);
     }
 }
