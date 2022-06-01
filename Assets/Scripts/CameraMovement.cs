77e3e8cea9dfe94e296b1558be7f962fe96ce278
rengetsu <rengetsu@mail.ru>
2022-06-01 23:10:13 +0300

    Resolve problems with moving to new machine

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -12,15 +12,15 @@ public class CameraMovement : MonoBehaviour
     /// <summary>
     /// How fast the camera can scroll up and down
     /// </summary>
-    float zoomSpeed = 10.0;
+    float zoomSpeed = 10.0f;
 
     /// <summary>
     /// How fast the camera can spin around on it's axis
     /// </summary>
-    float rotateSpeed = 0;
+    float rotateSpeed = 0.0f;
 
-    float maxHeight = 40f;      //  How high camera can go 
-    float minHeight = 4f;       //  How low camera can go
+    float maxHeight = 40.0f;      //  How high camera can go 
+    float minHeight = 4.0f;       //  How low camera can go
 
     // Start is called before the first frame update
     void Start()
@@ -39,7 +39,7 @@ public class CameraMovement : MonoBehaviour
         Vector3 verticalMove = new Vector3(0, scrollSp, 0);
         Vector3 lateralMove = hsp * transform.right;
         Vector3 forwardMove = transform.forward;
-        forwardMove.y = 0;
+        forwardMove.y = 0.0f;
         forwardMove.Normalize();
         forwardMove *= vsp;
 
