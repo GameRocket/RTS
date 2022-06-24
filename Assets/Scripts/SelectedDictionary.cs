d2f4f3d67ae7ac66cc6c648db505f18cbce6d949
rengetsu <rengetsu@mail.ru>
2022-06-24 23:46:44 +0300

    Selected Dictionary methods added

--- Assets/Scripts/SelectedDictionary.cs
+++ Assets/Scripts/SelectedDictionary.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SelectedDictionary : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Dictionary<int, GameObject> selectedTable = new Dictionary<int, GameObject>();
+
+    public void addSelected(GameObject go)
+    { 
+        int id = go.GetInstanceID();
+
+        if(!(selectedTable.ContainsKey(id)))
+        {
+            selectedTable.Add(id, go);
+            go.AddComponent<SelectedDictionary>();
+            Debug.Log("Added " + id + " to selected dict");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void deselect(int id)
     {
-        
+        Destroy(selectedTable[id].GetComponent<SelectedDictionary>());
+        selectedTable.Remove(id);
+    }
+
+    public void deselectAll()
+    {
+        foreach(KeyValuePair<int, GameObject> pair in selectedTable)
+        {
+            if(pair.Value != null)
+            {
+                Destroy(selectedTable[pair.Key].GetComponent<selection_component>);
+            }
+        }
+        selectedTable.Clear();
     }
 }
