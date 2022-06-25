05bee9b74b4066c4e4b44a1aa7ed075e1f4108e4
rengetsu <rengetsu@mail.ru>
2022-06-25 23:38:17 +0300

    Selection Global finished

--- Assets/Scripts/Selection/SelectionGlobal.cs
+++ Assets/Scripts/Selection/SelectionGlobal.cs
@@ -9,15 +9,26 @@ public class SelectionGlobal : MonoBehaviour
 
     bool dragSelect;
 
-    //  Colider variables
+    //Collider variables
+    //=======================================================//
+
+    MeshCollider selectionBox;
+    Mesh selectionMesh;
+
     Vector3 p1;
     Vector3 p2;
+
+    //the corners of our 2d selection box
+    Vector2[] corners;
+
+    //the vertices of our meshcollider
+    Vector3[] verts;
+    Vector3[] vecs;
 
     // Start is called before the first frame update
     void Start()
     {
-        id_table = GetComponent<id_dictionary>();
-        selected_table = GetComponent<selected_dictionary>();
+        selected_table = GetComponent<SelectedDictionary>();
         dragSelect = false;
     }
 
@@ -33,7 +44,7 @@ public class SelectionGlobal : MonoBehaviour
         //  2. While left mouse button held
         if ( Input.GetMouseButton(0) )
         {
-            if((p1 - Input.moousePosition).magnitude > 40)
+            if((p1 - Input.mousePosition).magnitude > 40)
             {
                 dragSelect = true;
             }
@@ -74,6 +85,102 @@ public class SelectionGlobal : MonoBehaviour
                 }
             }    
         }
+        else //marquee select
+        {
+            verts = new Vector3[4];
+            vecs = new Vector3[4];
+            int i = 0;
+            p2 = Input.mousePosition;
+            corners = getBoundingBox(p1, p2);
+
+            foreach (Vector2 corner in corners)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(corner);
+
+                if (Physics.Raycast(ray, out hit, 50000.0f, (1 << 8)))
+                {
+                    verts[i] = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+                    vecs[i] = ray.origin - hit.point;
+                    Debug.DrawLine(Camera.main.ScreenToWorldPoint(corner), hit.point, Color.red, 1.0f);
+                }
+                i++;
+            }
+
+            //generate the mesh
+            selectionMesh = generateSelectionMesh(verts, vecs);
+
+            selectionBox = gameObject.AddComponent<MeshCollider>();
+            selectionBox.sharedMesh = selectionMesh;
+            selectionBox.convex = true;
+            selectionBox.isTrigger = true;
+
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                selected_table.deselectAll();
+            }
+
+            Destroy(selectionBox, 0.02f);
+
+        }
+        //end marquee select
+
+        dragSelect = false;
+    }
+
+    private void OnGUI()
+    {
+        if (dragSelect == true)
+        {
+            var rect = ScreenUtility.GetScreenRect(p1, Input.mousePosition);
+            ScreenUtility.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+            ScreenUtility.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
+        }
+    }
+    
+    //create a bounding box (4 corners in order) from the start and end mouse position
+    Vector2[] getBoundingBox(Vector2 p1, Vector2 p2)
+    {
+        // Min and Max to get 2 corners of rectangle regardless of drag direction.
+        var bottomLeft = Vector3.Min(p1, p2);
+        var topRight = Vector3.Max(p1, p2);
         
+        // 0 = top left; 1 = top right; 2 = bottom left; 3 = bottom right;
+        Vector2[] corners =
+        {
+            new Vector2(bottomLeft.x, topRight.y),
+            new Vector2(topRight.x, topRight.y),
+            new Vector2(bottomLeft.x, bottomLeft.y),
+            new Vector2(topRight.x, bottomLeft.y)
+        };
+        
+        return corners;
+    }
+    
+    //generate a mesh from the 4 bottom points
+    Mesh generateSelectionMesh(Vector3[] corners, Vector3[] vecs)
+    {
+        Vector3[] verts = new Vector3[8];
+        int[] tris = { 0, 1, 2, 2, 1, 3, 4, 6, 0, 0, 6, 2, 6, 7, 2, 2, 7, 3, 7, 5, 3, 3, 5, 1, 5, 0, 1, 1, 4, 0, 4, 5, 6, 6, 5, 7 }; //map the tris of our cube
+        
+        for (int i = 0; i < 4; i++)
+        {
+            verts[i] = corners[i];
+        }
+        
+        for (int j = 4; j < 8; j++)
+        {
+            verts[j] = corners[j - 4] + vecs[j - 4];
+        }
+        
+        Mesh selectionMesh = new Mesh();
+        selectionMesh.vertices = verts;
+        selectionMesh.triangles = tris;
+        
+        return selectionMesh;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        selected_table.addSelected(other.gameObject);
     }
 }
